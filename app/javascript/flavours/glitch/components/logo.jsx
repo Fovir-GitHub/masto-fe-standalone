67a26e793affc0325e1f515f4820cd7d48f76aa4
julia <midnight@trainwit.ch>
2024-12-26 14:44:47 +1100

    [bugfix] Make the logo icon / wordmark on mobile/simple UI appear
    At least for the logo-symbol-wordmark.svg, this appears to usually be added
    by some ruby code in application.html.haml, but since we have a static index.html
    this never gets inserted into the HTML. Rather than embedding it into the HTML,
    just make the two logos pull from files in public/.
    
    This does not update the branding of the files, just makes them appear.

--- app/javascript/flavours/glitch/components/logo.jsx
+++ app/javascript/flavours/glitch/components/logo.jsx
@@ -1,14 +1,12 @@
-import logo from 'mastodon/../images/logo.svg';
-
 export const WordmarkLogo = () => (
   <svg viewBox='0 0 261 66' className='logo logo--wordmark' role='img'>
     <title>Mastodon</title>
-    <use xlinkHref='#logo-symbol-wordmark' />
+    <use xlinkHref='/logo-symbol-wordmark.svg#logo-symbol-wordmark' />
   </svg>
 );
 
 export const SymbolLogo = () => (
-  <img src={logo} alt='Mastodon' className='logo logo--icon' />
+  <img src='/logo-icon.svg' alt='Mastodon' className='logo logo--icon' />
 );
 
 export default WordmarkLogo;
