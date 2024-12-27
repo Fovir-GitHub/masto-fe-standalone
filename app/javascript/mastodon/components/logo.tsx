3481816b99ec9fb44e08cf0a5499ee3e2b78cc1c
julia <midnight@trainwit.ch>
2024-12-27 11:55:03 +0000

    [bugfix] Make the logo icon / wordmark on mobile/simple UI appear (#4)
    At least for the logo-symbol-wordmark.svg, this appears to usually be added
    by some ruby code in application.html.haml, but since we have a static index.html
    this never gets inserted into the HTML. Rather than embedding it into the HTML,
    just make the two logos pull from files in public/.
    
    This does not update the branding of the files, just makes them appear.
    
    Reviewed-on: https://codeberg.org/superseriousbusiness/masto-fe-standalone/pulls/4
    Co-authored-by: julia <midnight@trainwit.ch>
    Co-committed-by: julia <midnight@trainwit.ch>

--- app/javascript/mastodon/components/logo.tsx
+++ app/javascript/mastodon/components/logo.tsx
@@ -1,12 +1,10 @@
-import logo from 'mastodon/../images/logo.svg';
-
 export const WordmarkLogo: React.FC = () => (
   <svg viewBox='0 0 261 66' className='logo logo--wordmark' role='img'>
     <title>Mastodon</title>
-    <use xlinkHref='#logo-symbol-wordmark' />
+    <use xlinkHref='/logo-symbol-wordmark.svg#logo-symbol-wordmark' />
   </svg>
 );
 
 export const SymbolLogo: React.FC = () => (
-  <img src={logo} alt='Mastodon' className='logo logo--icon' />
+  <img src='/logo-icon.svg' alt='Mastodon' className='logo logo--icon' />
 );
